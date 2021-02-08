7e61ff803d97ba596d7be016586e9d2eac360f85
Mohammad Hossein Golestan <mohammad.h.golestan@ntnu.no>
2021-02-08 16:11:35 +0100

    css fixed

--- src/App.js
+++ src/App.js
@@ -1,62 +1,77 @@
-import React, { useState } from 'react';
-import ReactGlobe from 'react-globe';
-import defaultMarkers from './markers';
-import "./App.css"
+import React, { useState } from "react";
+import ReactGlobe from "react-globe";
+import "./App.css";
 
 const api = {
   key: "f23aee746d1da84801efc50791300a95",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
-
-
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function getTooltipContent(marker) {
   return `CITY: ${marker.city} (Value: ${marker.value})`;
 }
 
-
 function App() {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
 
-
-  const search = evt => {
+  const search = (evt) => {
     if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result => {
+        .then((res) => res.json())
+        .then((result) => {
+          if (result.cod !== 200) {
+            alert("city does not exist.");
+            return;
+          }
+
           setWeather(result);
-          setQuery('');
-          setMarkers([{
-            id: 1,
-            city: query,
-            color: `hsl(100, 100%, 50%)`,
-            coordinates: [result.coord.lat, result.coord.lon],
-            value: [Math.floor(result.main.temp)],
-          }])
+          setQuery("");
+          setMarkers([
+            {
+              id: 1,
+              city: query,
+              color: `hsl(100, 100%, 50%)`,
+              coordinates: [result.coord.lat, result.coord.lon],
+              value: [Math.floor(result.main.temp)],
+            },
+          ]);
         });
     }
-  }
+  };
 
   const dateBuilder = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
 
     let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
     let year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`
-  }
-
-
-  const randomMarkers = defaultMarkers.map(marker => ({
-    ...marker,
-    value: Math.floor(Math.random() * 100),
-  }));
-
-
+    return `${day} ${date} ${month} ${year}`;
+  };
 
   const [markers, setMarkers] = useState([]);
   const [event, setEvent] = useState(null);
@@ -64,7 +79,7 @@ function App() {
 
   function onClickMarker(marker, markerObject, event) {
     setEvent({
-      type: 'CLICK',
+      type: "CLICK",
       marker,
       markerObjectID: markerObject.uuid,
       pointerEventPosition: { x: event.clientX, y: event.clientY },
@@ -74,7 +89,7 @@ function App() {
 
   function onDefocus(previousCoordinates, event) {
     setEvent({
-      type: 'DEFOCUS',
+      type: "DEFOCUS",
       previousCoordinates,
       pointerEventPosition: { x: event.clientX, y: event.clientY },
     });
@@ -88,76 +103,64 @@ function App() {
           type="text"
           className="search-bar"
           placeholder="Search..."
-          onChange={e => setQuery(e.target.value)}
+          onChange={(e) => setQuery(e.target.value)}
           value={query}
           onKeyPress={search}
         />
       </div>
-      {(typeof weather.main != "undefined") ? (
-        <div>
-          <div className="location-box">
-            <div className="location">{weather.name}, {weather.sys.country}</div>
-            <div className="date">{dateBuilder(new Date())}</div>
+      {typeof weather.main != "undefined" ? (
+        <div className="result-box">
+          <div>
+            <div>
+              {weather.name}, {weather.sys.country}
+            </div>
+            <div>{dateBuilder(new Date())}</div>
           </div>
-          <div className="weather-box">
-            <div className="temp">
-              {Math.round(weather.main.temp)}°c
-            </div>
-            <div className="temp">
-              {weather.coord.lat}
-            </div>
-            <div className="weather">{weather.weather[0].main}</div>
+          <div>
+            <div>{Math.round(weather.main.temp)}°c</div>
+            <div>{weather.coord.lat}</div>
+            <div>{weather.weather[0].main}</div>
           </div>
         </div>
-      ) : ('')}
+      ) : (
+        ""
+      )}
 
-
-
-
-      <div className="React-Globe" >
+      <div className="React-Globe">
         <ReactGlobe
-          focus={weather.coord ? [weather.coord.lat, weather.coord.lon] : undefined}
+          focus={
+            weather.coord ? [weather.coord.lat, weather.coord.lon] : undefined
+          }
           markers={markers}
           markerOptions={{
             enableTooltip: true,
-            getTooltipContent: marker => `City: ${marker.city} (Temperature: ${marker.value} °c)`,
+            getTooltipContent: (marker) =>
+              `City: ${marker.city} (Temperature: ${marker.value} °c)`,
           }}
           onClickMarker={onClickMarker}
           onDefocus={onDefocus}
         />
 
-
         {details && (
           <div
             style={{
-              background: 'white',
-              position: 'absolute',
-              fontSize: 20,
+              background: "white",
+              position: "absolute",
+              fontSize: 200,
               top: 0,
               right: 0,
               padding: 0,
-            }}>
+            }}
+          >
             <p>{details}</p>
             <p>
               EVENT: type={event.type}, position=
-          {JSON.stringify(event.pointerEventPosition)})
-        </p>
+              {JSON.stringify(event.pointerEventPosition)})
+            </p>
           </div>
         )}
-
-
-        <button
-          disabled={markers.length === 0}
-          onClick={() => setMarkers(markers.slice(0, markers.length - 1))}>
-          Local Weather
-        </button>
-
-
-
-
-
       </div>
-    </div >
+    </div>
   );
 }
 
